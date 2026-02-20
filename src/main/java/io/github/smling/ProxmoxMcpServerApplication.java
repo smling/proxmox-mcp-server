f9a07fbc84a5b4cc9e3650cb72581494412efe0e
ch-li <smling9_9@hotmail.com>
2026-02-20 21:03:25 +0000

    initial commit

---
+++ src/main/java/io/github/smling/ProxmoxMcpServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.smling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxmoxMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxmoxMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/smling/ProxmoxMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.smling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxmoxMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
