8c3f41ec556f6281a6c5bd30d0a2c6085fbb2667
ch-li <smling9_9@hotmail.com>
2026-02-21 20:19:31 +0000

    1- add exception handler

---
+++ src/main/java/io/github/smling/proxmoxmcpserver/spring/ApiExceptionHandler.java
@@ -0,0 +1,19 @@
+package io.github.smling.proxmoxmcpserver.spring;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleServerError(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+}
